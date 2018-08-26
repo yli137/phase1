2cab63c5bff1f3304c92ae6cc360df35d26b8cfc
Yicheng Li <yli137@saturn.icl.utk.edu>
2018-08-26 12:48:43 -0400

    data

--- hint_test/hintT0mem/runhint.sh
+++ hint_test/hintT0mem/runhint.sh
@@ -1,40 +1,40 @@
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./1hintT0 $i $(($i*8)) >> 1hintT0.txt;
+	mpirun -np 1 ./1hintT0 $i $(($i*8)) >> 1hintT0.txt;
 done
 
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./2hintT0 $(($i*2)) $(($i*8)) >> 2hintT0.txt;
+	mpirun -np 1 ./2hintT0 $(($i*2)) $(($i*8)) >> 2hintT0.txt;
 done
 
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./3hintT0 $(($i*3)) $(($i*8)) >> 3hintT0.txt;
+	mpirun -np 1 ./3hintT0 $(($i*3)) $(($i*8)) >> 3hintT0.txt;
 done
 
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./4hintT0 $(($i*4)) $(($i*8)) >> 4hintT0.txt;
+	mpirun -np 1 ./4hintT0 $(($i*4)) $(($i*8)) >> 4hintT0.txt;
 done
 
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./5hintT0 $(($i*5)) $(($i*8)) >> 5hintT0.txt;
+	mpirun -np 1 ./5hintT0 $(($i*5)) $(($i*8)) >> 5hintT0.txt;
 done
 
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./6hintT0 $(($i*6)) $(($i*8)) >> 6hintT0.txt;
+	mpirun -np 1 ./6hintT0 $(($i*6)) $(($i*8)) >> 6hintT0.txt;
 done
 
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./7hintT0 $(($i*7)) $(($i*8)) >> 7hintT0.txt;
+	mpirun -np 1 ./7hintT0 $(($i*7)) $(($i*8)) >> 7hintT0.txt;
 done
 
 for i in {1000..500000..1000}; 
 do
-	mpirun -np 20 ./8hintT0 $(($i*8)) $(($i*8)) >> 8hintT0.txt;
+	mpirun -np 1 ./8hintT0 $(($i*8)) $(($i*8)) >> 8hintT0.txt;
 done
 
