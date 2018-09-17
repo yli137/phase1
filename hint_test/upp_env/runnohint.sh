074f264e3b7e1efbe9f0a61c4e468812cefc37d6
Yicheng Li <yli137@saturn.icl.utk.edu>
2018-09-17 12:06:37 -0400

    data change

--- hint_test/upp_env/runnohint.sh
+++ hint_test/upp_env/runnohint.sh
@@ -2,6 +2,6 @@ for num in {1..8..1}
 do
 	for i in {40000..560000..40000}
 	do
-		mpirun -n 1 ./upp_nohint $(($i * $num)) $(($i * 8)) $num 8 >> nohintdata.txt
+		mpirun -n 1 ./upp_nohint $(($i * $num)) $(($i * 8)) $num 8 >> nohintdata2.txt
 	done
 done
